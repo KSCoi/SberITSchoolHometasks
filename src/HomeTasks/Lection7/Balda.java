ec962871e9a8f52c20a8eafcb4d5ac42c39aca95
Konstantin <tsoykonstser@gmail.com>
2021-04-20 11:36:59 +0300

    add some comments for balda game

--- src/HomeTasks/Lection7/Balda.java
+++ src/HomeTasks/Lection7/Balda.java
@@ -8,7 +8,7 @@ public class Balda {
 
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
-        var player1 = new Player();
+        var player1 = new Player(); // создаем двух игроков
         var player2 = new Player();
 
         System.out.println("Please input the main word");
@@ -24,8 +24,8 @@ public class Balda {
             letters.add(mainWord.charAt(i));
         }
 
-        while(!(player1.isSurrender && player2.isSurrender)){
-            if(turn%2!=0)
+        while(!(player1.isSurrender && player2.isSurrender)){  // проверка сдался игрок или нет (ввел ли пустую строку)
+            if(turn%2!=0) // проверка на порядок хода
             {
                 System.out.println("turn of Player 1");
             }
@@ -33,31 +33,31 @@ public class Balda {
             String playerWord = scanner.nextLine();
 
             if(turn%2!=0) {
-                if (playerWord.isEmpty())   {
+                if (playerWord.isEmpty())   { //если игрок 1 ввел пустую строку, то он сдается, продолжаем со вторым
                     player1.isSurrender = true;
                     turn++;
                     continue;
                 }
-                if(!isPartOfMainWord(playerWord,letters)){
+                if(!isPartOfMainWord(playerWord,letters)){ //проверка на буквы
                     System.out.println("Your new word consists letters, that are not contained it the word: " + mainWord);
                     System.out.println("Enter new word");
                     continue;
                 }
-                if(!words.add(playerWord))
+                if(!words.add(playerWord)) //проверка на наличие слова
                 {
                     System.out.println("The word: "+"<<"+ playerWord+">>"+" has already  been used, please try another word or press ENTER to give up");
                     continue;
                 }
 
-                turn++;
-                if(player2.isSurrender)
+                turn++; //если все предыдущие пункты пройдены, то ход следующего
+                if(player2.isSurrender) // если следующий сдался, то ход остается у игрока 1
                 {
                     turn--;
                 }
                 wordsOfPlayer1.add(playerWord);
                 continue;
             }
-            if(turn%2==0){
+            if(turn%2==0){ // Все тоже самое, только для игрока 2
                 if(playerWord.isEmpty())    {
                     player2.isSurrender=true;
                     turn++;
@@ -92,14 +92,12 @@ public class Balda {
             System.out.println("DRAW!");
     }
 
-    public static boolean isPartOfMainWord(String playerWord,HashSet<Character> mainWordLetters){
+    public static boolean isPartOfMainWord(String playerWord,HashSet<Character> mainWordLetters){ // сравниваем буквы mainWord и playerWord
         char[] arr = playerWord.toCharArray();
-        boolean tmp = false;
         for(var letter : arr){
-            tmp = false;
-            if (mainWordLetters.contains(letter))
-                tmp = true;
+            if (!mainWordLetters.contains(letter))
+                return  false;
         }
-        return tmp;
+        return true;
     }
 }
