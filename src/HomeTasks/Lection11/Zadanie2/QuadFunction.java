388712c9de6039f058073a9b6aeefd38483ad1bb
Konstantin <tsoykonstser@gmail.com>
2021-05-08 11:25:56 +0300

    Add IntersectOfList,PositiveStr,AvgWeight,NameList

--- src/HomeTasks/Lection11/Zadanie2/QuadFunction.java
+++
@@ -1,8 +0,0 @@
-package HomeTasks.Lection11.Zadanie2;
-
-import java.util.function.Function;
-
-public interface QuadFunction <T,U,S,V,R> {
-
-    R apply(T t, U u, S s, V v);
-}
--- src/HomeTasks/Lection11/Zadanie2/TriFunction.java
+++
@@ -1,6 +0,0 @@
-package HomeTasks.Lection11.Zadanie2;
-
-public interface TriFunction<T,U,S,R> {
-
-    R apply(T t,U u,S s);
-}
